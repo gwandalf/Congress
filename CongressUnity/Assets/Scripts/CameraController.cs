b524770a3da51e34560dd88370dcfde9c75c72c7
gwandalf <gwlemoul@insa-rennes.fr>
2015-12-03 22:38:23 +0100

    camera translation ok

---
+++ CongressUnity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Translate(x, y);
+	}
+
+    /// <summary>
+    /// Translates the camera over the horizontal plan.
+    /// </summary>
+    /// <param name="x">x value</param>
+    /// <param name="y">y value</param>
+    void Translate(float x, float y)
+    {
+        this.gameObject.transform.position += new Vector3(x, 0, y);
+    }
+}
