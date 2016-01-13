cfe27833fe5d058554e44a78fa1d48462bbcfcbe
Gwendal Le Moulec <glemoule@ad.inria.fr>
2016-01-13 19:22:56 +0100

    resize ok

--- CongressUnity2D/Assets/Scripts/Model/PersonSetModel.cs
+++ CongressUnity2D/Assets/Scripts/Model/PersonSetModel.cs
@@ -15,7 +15,7 @@ public class PersonSetModel : MonoBehaviour {
     /// <summary>
     /// Person set.
     /// </summary>
-    private HashSet<GameObject> persons;
+    private List<GameObject> persons;
 
     /// <summary>
     /// Person Prefab.
@@ -24,9 +24,22 @@ public class PersonSetModel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        persons = new HashSet<GameObject>();
+        persons = new List<GameObject>();
         createPersons(initialSize);
 	}
+
+    /// <summary>
+    /// Update the person number to the specified one.
+    /// </summary>
+    /// <param name="number">New person number.</param>
+    public void resize(int number)
+    {
+        if (number > persons.Count)
+            createPersons(number - persons.Count);
+        else if (number < persons.Count)
+            deletePersons(persons.Count - number);
+        else return;
+    }
 	
     /// <summary>
     /// Creates a number of persons equal to the specified parameter.
@@ -40,6 +53,21 @@ public class PersonSetModel : MonoBehaviour {
             float y = Random.value * 10 - 5;
             GameObject go = (GameObject)Instantiate(personPrefab, new Vector3(x, y, 0), Quaternion.identity);
             persons.Add(go);
+        }
+    }
+
+    /// <summary>
+    /// Deletes a number of persons equal to the specified parameter.
+    /// </summary>
+    /// <param name="number">Number of persons to delete.</param>
+    public void deletePersons(int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            int index = (int) (Random.value * (persons.Count - 1));
+            GameObject go = persons[index];
+            persons.Remove(go);
+            Destroy(go);
         }
     }
 
---
+++ CongressUnity2D/Assets/Scripts/View/PersonNumberConfig.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Manages the person number UI.
+/// </summary>
+public class PersonNumberConfig : MonoBehaviour {
+
+    /// <summary>
+    /// GameObject holding the person set.
+    /// </summary>
+    public GameObject personSetHolder;
+
+    public GameObject field;
+
+    /// <summary>
+    /// The observed PersonSetModel.
+    /// </summary>
+    private PersonSetModel observed;
+
+	// Use this for initialization
+	void Start () {
+        observed = (PersonSetModel)personSetHolder.GetComponent(typeof(PersonSetModel));
+	}
+	
+    /// <summary>
+    /// Draws a field and a validation button.
+    /// </summary>
+	public void OnClick()
+    {
+        observed.resize(2);
+    }
+
+}
